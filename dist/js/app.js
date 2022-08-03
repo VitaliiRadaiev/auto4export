c50fa2bf72fe2a7b96399ad99851532f2e959928
Spacespire <aleksandra@spacespire.com>
2022-08-03 16:28:04 +0300

    icons fixed

--- dist/js/app.js
+++ dist/js/app.js
@@ -136,43 +136,25 @@ class Utils {
 	}
 
 	replaceToInlineSvg(query) {
-		let map = new Map();
 		const images = document.querySelectorAll(query);
-	
-		const forEach = (i) => {
-			if(i < 0) {
-				return
-			} else {
-				let svg = map.get(images[i].src);
-				if (svg) {
-					images[i].parentNode.replaceChild(svg.cloneNode(true), images[i]);
-					forEach(i - 1)
-	
-				} else {
+
+		if(images.length) {
+			images.forEach(img => {
 					let xhr = new XMLHttpRequest();
-					xhr.open('GET', images[i].src);
-		
+					xhr.open('GET', img.src);
 					xhr.onload = () => {
 						if (xhr.readyState === xhr.DONE) {
 							if (xhr.status === 200) {
-
 								let svg = xhr.responseXML.documentElement;
-								images[i].parentNode.replaceChild(svg, images[i]);
-								map.set(images[i].src, svg);
-								forEach(i - 1)
-
-							} else {
-								forEach(i - 1)
+								svg.classList.add('_svg');
+								img.parentNode.replaceChild(svg, img);
+								map.set(img.src, svg);
 							}
 						}
 					}
 					xhr.send(null);
-				}
-				
-			}
+			})
 		}
-	
-		forEach(images.length -1);
 	}
 
 	setSameHeight() {
