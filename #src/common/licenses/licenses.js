06326644550ddd86d5592eb0150cc64b6b7ab00f
Spacespire <aleksandra@spacespire.com>
2022-10-12 13:43:15 +0300

    fix

--- #src/common/licenses/licenses.js
+++ #src/common/licenses/licenses.js
@@ -2,13 +2,31 @@
     let licensesSlider = document.querySelector('[data-slider="licenses-slider"]');
     if(licensesSlider) {
         let sliderData = new Swiper(licensesSlider.querySelector('.swiper'), {
-            slidesPerView: 'auto',
-            spaceBetween: 0,
             speed: 600,
             navigation: {
                 nextEl: licensesSlider.querySelector('.slider-btn.btn-next'),
                 prevEl: licensesSlider.querySelector('.slider-btn.btn-prev'),
             },
+            breakpoints: {
+                0: {
+                    slidesPerView: 'auto',
+                    spaceBetween: 0,
+                    autoHeight: false,
+                    freeMode: true,
+                },
+                768: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                    autoHeight: true,
+                    freeMode: false,
+                },
+                992: {
+                    slidesPerView: 'auto',
+                    spaceBetween: 0,
+                    autoHeight: false,
+                    freeMode: false,
+                }
+            }
         });
     }
 }
